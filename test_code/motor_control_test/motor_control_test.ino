ddc552952a13a6e995ef29e381fb43651f7b8664
Keepfiring <cwallis@gmail.com>
2019-01-25 13:30:26 -0800

    Create motor_control_test.ino
    created test sketch to check motor command functionality

---
+++ test_code/motor_control_test/motor_control_test.ino
@@ -0,0 +1,38 @@
+/* Test code to check functionality of digitial potentiometer, 
+ * flow enable/disable relay, and speed control
+ * 
+  */
+
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+int motorCommand = 0;
+
+void setup() {
+  
+  Serial.begin(9600);
+  Wire.begin(); // join i2c bus (address optional for master)
+  pinMode(6, OUTPUT); //motor enable pin
+  digitalWrite(6, HIGH);    
+  
+
+}
+
+void loop() {
+  //Serial.println("motor_on");
+  //digitalWrite(6, HIGH);
+  
+  //Digital Potentiometer:
+  Wire.beginTransmission(44); // 
+  // device address is specified in datasheet
+  Wire.write(byte(16));            // sends instruction byte
+  Wire.write(motorCommand);             // sends potentiometer value byte
+  Wire.endTransmission();     // stop transmitting
+  Serial.print("Motor command = ");
+  Serial.println(motorCommand);
+  delay(1000);
+
+  motorCommand += 5;
+  if (motorCommand > 255) motorCommand = 0;
+
+}
